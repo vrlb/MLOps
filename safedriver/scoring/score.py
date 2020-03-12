c2413e52833b20b728475a66ef5310ef01407036
vrlb <verlebie@outlook.com>
2020-03-12 09:27:05 +0800

    tidy up files

--- safedriver/scoring/score.py
+++ safedriver/scoring/score.py
@@ -1,28 +1,3 @@
-"""
-Copyright (C) Microsoft Corporation. All rights reserved.​
- ​
-Microsoft Corporation (“Microsoft”) grants you a nonexclusive, perpetual,
-royalty-free right to use, copy, and modify the software code provided by us
-("Software Code"). You may not sublicense the Software Code or any use of it
-(except to your affiliates and to vendors to perform work on your behalf)
-through distribution, network access, service agreement, lease, rental, or
-otherwise. This license does not purport to express any claim of ownership over
-data you may have shared with Microsoft in the creation of the Software Code.
-Unless applicable law gives you more rights, Microsoft reserves all other
-rights not expressly granted herein, whether by implication, estoppel or
-otherwise. ​
- ​
-THE SOFTWARE CODE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS
-OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-MICROSOFT OR ITS LICENSORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-ARISING IN ANY WAY OUT OF THE USE OF THE SOFTWARE CODE, EVEN IF ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-"""
 import numpy
 import joblib
 import os
@@ -47,9 +22,23 @@ def init():
     model = joblib.load(model_path)
 
 
-input_sample = numpy.array([
-    [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
-    [10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]])
+input_sample = numpy.array([[
+                            0,1,8,1,0,0,1,0,0,0,0,0,
+                            0,0,12,1,0,0,0.5,
+                            0.3,0.610327781,7,1,-1,0,
+                            -1,1,1,1,2,1,65,1,
+                            0.316227766,0.669556409,
+                            0.352136337,3.464101615,
+                            0.1,0.8,0.6,1,1,6,3,6,2,9,
+                            1,1,1,12,0,1,1,0,0,1],
+                            [4,2,5,1,0,0,0,0,1,0,0,0,
+                            0,0,5,1,0,0,0.9,0.5,
+                            0.771362431,4,1,-1,0,0,11,1,
+                            1,0,1,103,1,0.316227766,
+                            0.60632002,0.358329457,
+                            2.828427125,0.4,
+                            0.5,0.4,3,3,8,4,
+                            10,2,7,2,0,3,10,0,0,1,1,0,1]])
 output_sample = numpy.array([
     5021.509689995557,
     3693.645386402646])
